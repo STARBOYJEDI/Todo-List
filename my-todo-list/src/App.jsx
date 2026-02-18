455e9509d5d026ae64fb52031fad8a92d53a825b
Mbuso Mabuza <mbusomabuza0613@gmail.com>
2026-02-19 00:23:04 +0200

    - Created a const array for todos items - Created a function that handles the addition of new items whenever the user inputs a string in the app

--- my-todo-list/src/App.jsx
+++ my-todo-list/src/App.jsx
@@ -3,9 +3,24 @@ import "./style.css"
 
 export default function App() {
   const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ]
+    })
+  }
+
+
+
   return (
     <>
-      <form className="new-item-form">
+      <form onSubmit={handleSubmit} className="new-item-form">
       <div className="form-row">
         <label htmlFor="item">New Item</label>
         <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item" />
@@ -14,20 +29,15 @@ export default function App() {
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 1
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 2
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
+        {todos.map(todo => {
+          return <li>
+            <label>
+              <input type="checkbox" />
+              {todo.title}
+            </label>
+            <button className="btn btn-danger">Delete</button>
+          </li>
+        })}
       </ul>
     </>
   )
